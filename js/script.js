994a7b4bb2117026e367a1cf9732523d1490d66c
sophie-nguyen-0 <sophie-nguyen-0@users.noreply.github.com>
2022-12-16 13:21:45 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -18,15 +18,29 @@ const checkWeather = async (URLAddress) => {
     const result = await fetch(URLAddress)
     const data = await result.json()
     console.log(data)
-    document.getElementById('api-weather-icon').innerHTML = '<img src="' + 
-      "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png" + 
-      '" alt="API image" class="center" ' + ' width="20%" height="20%"'+
-      '>' 
-    document.getElementById('api-weather').innerHTML = '<h3>' + data.weather[0].description +'</h3>'+'<br>'+'<h4> temperature: '+(data.main.temp - 273.15).toFixed(2)+' feels like: '+(data.main.feels_like - 273.15).toFixed(2) +'</h4>'
+    document.getElementById("api-weather-icon").innerHTML =
+      '<img src="' +
+      "http://openweathermap.org/img/wn/" +
+      data.weather[0].icon +
+      "@2x.png" +
+      '" alt="API image" class="center" ' +
+      ' width="20%" height="20%"' +
+      ">"
+    document.getElementById("api-weather").innerHTML =
+      "<h3>" +
+      data.weather[0].description +
+      "</h3>" +
+      "<br>" +
+      "<h4> temperature: " +
+      (data.main.temp - 273.15).toFixed(2) +
+      " feels like: " +
+      (data.main.feels_like - 273.15).toFixed(2) +
+      "</h4>"
   } catch (error) {
     console.log(error)
   }
 }
 
-checkWeather("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
-
+checkWeather(
+  "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+)
